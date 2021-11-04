12e4d74e29e20b89841686852a858964813d686f
zhangfan <cyouho@hotmail.co.jp>
2021-11-04 08:38:31 +0800

    first commit

---
+++ entry/src/main/java/com/example/harmony_test/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.harmony_test;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
